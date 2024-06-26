17b9463b6a2978bb63c5d4e2a3a571227eea9bea
Nathan Totten <nathan.totten@kainos.com>
2024-06-26 15:53:39 +0100

    addedAdded script to create the user table

---
+++ src/main/resources/db/migration/V2__create_user_table.sql
@@ -0,0 +1,9 @@
+create table user (
+	user_id int NOT NULL AUTO_INCREMENT,
+    username VARCHAR(100) NOT NULL,
+    password VARCHAR(100) NOT NULL,
+    role_id int,
+    PRIMARY KEY (user_id),
+    CONSTRAINT fk_role_id FOREIGN KEY (role_id) REFERENCES role(id),
+    CONSTRAINT uc_uname UNIQUE (username)
+);
