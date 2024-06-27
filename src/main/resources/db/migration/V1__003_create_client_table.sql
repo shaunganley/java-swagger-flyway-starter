355d65afaad7d6b3fa592606bee17461fd9c5030
Katiemcallister <katie.mcallister@kainos.com>
2024-06-27 12:17:24 +0100

    Create client table script

---
+++ src/main/resources/db/migration/V1__003_create_client_table.sql
@@ -0,0 +1,8 @@
+CREATE TABLE `Client` (
+                            `Client_id` smallint,
+                            `Name` varchar(255),
+                            `Address` varchar(200),
+                            `Phone_Num` varchar(64),
+                            `Proj_id` smallint
+
+);
