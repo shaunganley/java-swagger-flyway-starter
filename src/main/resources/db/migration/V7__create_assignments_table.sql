7c2a169e1e9538bf8448c80ae67abe0d9acc9490
ntotten-kainos <nathan.totten@kainos.com>
2024-06-27 13:52:58 +0100

    Assignments table
    Table to map employees to their project(s)

---
+++ src/main/resources/db/migration/V7__create_assignments_table.sql
@@ -0,0 +1,7 @@
+create table Assignments (
+    project_id int not null,
+    emp_id int not null,
+    working tinyint(1),
+    FOREIGN KEY (project_id) REFERENCES Project(project_id),
+    FOREIGN KEY (emp_id) REFERENCES Employee(emp_id)
+);
