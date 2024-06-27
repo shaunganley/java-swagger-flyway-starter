e9d2007f0076fae250ba756483c7aa439ecce236
jackpollinsK <jackpollins@kainos.com>
2024-06-27 13:48:36 +0100

    add emp_table and emp_sales_table

--- src/main/resources/db/migration/V1__Create_Emp_Sales.sql
+++ src/main/resources/db/migration/V1__Create_Emp_Sales.sql
@@ -1,8 +1,10 @@
 
+
 CREATE TABLE Employee(
     EmployeeID smallint PRIMARY KEY AUTO_INCREMENT not NULL,
     FName varchar(30),
     SName varchar(60),
+    Salary Decimal(11,2),
     BankNumber tinyint,
     NiNumber char(8)
 );
