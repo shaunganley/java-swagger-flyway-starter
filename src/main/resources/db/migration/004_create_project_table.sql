9539830e8710e2251150aade0f633f6ac9ecd33c
ntotten-kainos <nathan.totten@kainos.com>
2024-06-27 11:27:53 +0100

    Create 004_create_project_table.sql

---
+++ src/main/resources/db/migration/004_create_project_table.sql
@@ -0,0 +1,9 @@
+create table Project (
+	project_id int auto_increment not null,
+    project_value decimal(12,2) not null, 
+    project_name VARCHAR(50),
+    project_description VARCHAR(100),
+    client_id int not null,
+    PRIMARY KEY(project_id),
+    FOREIGN KEY (client_id) REFERENCES Client(client_id)
+);
