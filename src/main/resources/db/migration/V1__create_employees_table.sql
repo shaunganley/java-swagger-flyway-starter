8729b18631807a8dca5272c1082cea97394a49a8
Jamie McConnell <jamie.mcconnell@kainos.com>
2024-06-27 12:56:41 +0100

    Update employees table

--- src/main/resources/db/migration/V1__create_employees_table.sql
+++ src/main/resources/db/migration/V1__create_employees_table.sql
@@ -1,11 +1,11 @@
 
 
-create table Employees (
+create table employees (
     employee_ID smallint Primary Key AUTO_INCREMENT NOT NULL,
     employee_name varchar(70),
     employee_salary decimal(7,2),
-    employee_bank_account_number varchar(8),
-    employee_national_insurance_number varchar(9),
-    employee_commission_rate DECIMAL(2, 2),
-    employee_roleID smallint
+    employee_bankAccountNumber varchar(8),
+    employee_nationalInsuranceNumber varchar(9),
+    employee_commissionRate DECIMAL(2, 2),
+    role_ID smallint
 )
