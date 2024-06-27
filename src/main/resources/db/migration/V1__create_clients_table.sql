a4debd4f162743348022d701c6966f0327dd7f61
Thomas Sargent <thomas.sargent@kainos.com>
2024-06-27 12:53:30 +0100

    Updated clients variables

--- src/main/resources/db/migration/V1__create_clients_table.sql
+++ src/main/resources/db/migration/V1__create_clients_table.sql
@@ -2,6 +2,6 @@ create table clients(
     client_ID smallint PRIMARY KEY AUTO_INCREMENT NOT NULL,
     client_name varchar(30),
     client_address varchar(60),
-    client_phone_number char(11),
-    project_ID smallint
+    client_phoneNumber char(11),
+    projectID smallint
 )
