9ce0f97d5a0da17a218f340986ccbb86f138a02a
ntotten-kainos <nathan.totten@kainos.com>
2024-06-27 15:21:23 +0100

    Create script to populate client data

---
+++ src/main/resources/db/migration/V8__populate_client.sql
@@ -0,0 +1,8 @@
+insert into Client(client_name,building_no,street_name,postcode,city,county,country,phone_number)
+values ("Joe Bloggs", "123", "Bloggs St", "BT231YH", "Blogg City", "County Joe", "UK", 12345678911);
+
+insert into Client(client_name,building_no,street_name,postcode,city,county,country,phone_number)
+values ("Tim Apple", "54", "Apple St", "TA625XY", "Tim City", "County Tim", "UK", 09876543121);
+
+insert into Client(client_name,building_no,street_name,postcode,city,county,country,phone_number)
+values ("Mister Kainoos", "A4-A6", "Upper crescent", "BT231YH", "Kainoos Town", "Software State", "US of Kainoos", 23235642345);
