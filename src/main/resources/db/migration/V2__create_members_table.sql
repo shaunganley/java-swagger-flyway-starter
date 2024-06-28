59d1e050c21bf07d5ceb7d7cac1c651bb8147da4
rackie-pascua <rackie.pascua@kainos.com>
2024-06-28 15:39:39 +0100

    create loan table and populate

--- src/main/resources/db/migration/V2__create_members_table.sql
+++ src/main/resources/db/migration/V2__create_members_table.sql
@@ -9,6 +9,6 @@ CREATE TABLE `Members` (
 );
 
 INSERT INTO Members (first_name, last_name, address, phone, email, register_date) VALUES
-('Rackie', 'Pascua', '78-86 Dublin Road', '07389891341', 'rackiepascua@gmail.com', 28/06/24)
-('Zita', 'Cheung', '61 Fitzwilliam Street', '0731234567', 'zitacheung@gmail.com', 28/06/24)
+('Rackie', 'Pascua', '78-86 Dublin Road', '07389891341', 'rackiepascua@gmail.com', 28/06/24),
+('Zita', 'Cheung', '61 Fitzwilliam Street', '0731234567', 'zitacheung@gmail.com', 28/06/24),
 ('Jerico', 'Atos', '78-86 Dublin Road', '07298984131', 'rackiepascua@gmail.com', 28/06/24);
