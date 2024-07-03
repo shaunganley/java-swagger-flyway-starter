1bc2da12e628345794336a95b44274f7655e0863
Joshua Peter Noble <joshua.noble@hqp32341hc.home>
2024-07-03 15:48:39 +0100

    main-file

--- src/main/java/org/example/daos/TestDao.java
+++ src/main/java/org/example/daos/TestDao.java
@@ -17,12 +17,9 @@ public class TestDao {
             ResultSet resultSet = statement.executeQuery(
                     "SHOW DATABASES;");
 
-            System.out.println("Retrieving database names");
             while (resultSet.next()) {
-                System.out.println(resultSet.getString("Database"));
                 databases.add(resultSet.getString("Database"));
             }
-            System.out.println("Finished retrieving database names");
         }
 
         return databases;
