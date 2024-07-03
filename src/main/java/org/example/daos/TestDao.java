f21d138f3b7c1d9bf73de31da1c5834a295729e8
rackie-pascua <rackie.pascua@kainos.com>
2024-07-03 14:40:50 +0100

    Adding Logging

--- src/main/java/org/example/daos/TestDao.java
+++ src/main/java/org/example/daos/TestDao.java
@@ -11,18 +11,15 @@ public class TestDao {
         try (Connection connection = DatabaseConnector.getConnection()) {
             Statement statement = connection.createStatement();
 
-            long start = System.currentTimeMillis();
-
             ResultSet resultSet = statement.executeQuery(
                     "SHOW DATABASES;");
 
-          long end = System.currentTimeMillis();
-
-            System.out.println("Total time to execute query in milliseconds" + (end - start));
-
+            System.out.println("Retrieved databases names");
             while (resultSet.next()) {
+                System.out.println(resultSet.getString("Database"));
                 databases.add(resultSet.getString("Database"));
             }
+            System.out.println("Finished retrieving database naems");
         }
 
         return databases;
