af64ccce49e8be3c57da5e43e37d32af4393a2d0
rackie-pascua <rackie.pascua@kainos.com>
2024-07-03 13:50:39 +0100

    Adding logging to database list

--- src/main/java/org/example/daos/TestDao.java
+++ src/main/java/org/example/daos/TestDao.java
@@ -1,9 +1,6 @@
 package org.example.daos;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -17,11 +14,15 @@ public class TestDao {
             ResultSet resultSet = statement.executeQuery(
                     "SHOW DATABASES;");
 
+            System.out.println("Retrieving database names");
             while (resultSet.next()) {
+                System.out.println(resultSet.getString("Database"));
                 databases.add(resultSet.getString("Database"));
             }
+            System.out.println("Finished retrieving database names");
         }
 
         return databases;
     }
 }
+
