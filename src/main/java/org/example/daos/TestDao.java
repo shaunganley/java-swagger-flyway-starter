bd100897bddfbccc2559d545e4d0599eb6434f5e
PhilipGardinerK <philip.gardiner@Kainos.com>
2024-07-04 10:03:26 +0100

    Fixed Linter Issue from TestDao

--- src/main/java/org/example/daos/TestDao.java
+++ src/main/java/org/example/daos/TestDao.java
@@ -1,6 +1,10 @@
 package org.example.daos;
 
-import java.sql.*;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
