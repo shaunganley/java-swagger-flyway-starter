2114d57dc639b3ba50c12784783d13423600c8ab
ntotten-kainos <nathan.totten@kainos.com>
2024-07-04 11:33:32 +0100

    Insert dummy data script for sales employee

---
+++ src/main/resources/db/migration/Vx_insert_dud_sales_employee.sql
@@ -0,0 +1,14 @@
+INSERT INTO `SalesEmployee`(
+        Name,
+        BankAcctNum,
+        NINO,
+        Salary,
+        CommissionRate
+    )
+    VALUES (
+        "Joe Bloggs",
+        "12345678",
+        "AB123456C",
+        65000.00,
+        25.00
+    )
