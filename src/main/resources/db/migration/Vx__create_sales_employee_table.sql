0a422ebc6886eece2bf2e8504157c47c9d785367
ntotten-kainos <nathan.totten@kainos.com>
2024-07-04 11:36:23 +0100

    Fix typo

--- src/main/resources/db/migration/Vx__create_sales_employee_table.sql
+++ src/main/resources/db/migration/Vx__create_sales_employee_table.sql
@@ -3,7 +3,7 @@ CREATE TABLE `SalesEmployee` (
     Name VARCHAR(75) NOT NULL,
     BankAcctNum int NOT NULL,
     NINO CHAR(9) NOT NULL,
-    Salary decimal(9,2) NOT NULL
+    Salary decimal(9,2) NOT NULL,
     CommissionRate decimal (5,2)
 );
 
