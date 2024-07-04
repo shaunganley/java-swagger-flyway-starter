73ef9411dfdcf11b0e941db325b152c5412b79e0
hannahmorgan24 <hannah.morgan@kainos.com>
2024-07-04 15:45:25 +0100

    Creating project table

--- src/main/java/org/example/daos/ProjectDao.java
+++ src/main/java/org/example/daos/ProjectDao.java
@@ -13,25 +13,27 @@ public class ProjectDao {
 
     public int createProject(final ProjectRequest project) throws SQLException {
 
-        Connection c = DatabaseConnector.getConnection();
 
-        String insertStatement =
-                "INSERT INTO Project (projectName, "
-                        + "projectValue, clientID) VALUES (?,?,?)";
+        try (Connection c = DatabaseConnector.getConnection()) {
 
-        PreparedStatement st =
-                c.prepareStatement(insertStatement,
-                        Statement.RETURN_GENERATED_KEYS);
+            String insertStatement =
+                    "INSERT INTO Project (projectName, "
+                            + "projectValue, clientID) VALUES (?,?,?)";
 
-        st.setString(1, project.getProjectName());
-        st.setDouble(2, project.getProjectValue());
-        st.setInt(3, project.getClientID());
+            PreparedStatement st =
+                    c.prepareStatement(insertStatement,
+                            Statement.RETURN_GENERATED_KEYS);
 
-    st.executeUpdate();
-    ResultSet rs = st.getGeneratedKeys();
-    if (rs.next()) {
-        return rs.getInt(1);
-    }
+            st.setString(1, project.getProjectName());
+            st.setDouble(2, project.getProjectValue());
+            st.setInt(3, project.getClientID());
+
+            st.executeUpdate();
+            ResultSet rs = st.getGeneratedKeys();
+            if (rs.next()) {
+                return rs.getInt(1);
+            }
+        }
     return -1;
     }
 }
