9028ca53311eaa1fe7e73219c0871226b5455f96
PhilipGardinerK <philip.gardiner@Kainos.com>
2024-07-04 10:08:43 +0100

    Fixed more Linter issues

--- src/main/java/org/example/daos/TestDao.java
+++ src/main/java/org/example/daos/TestDao.java
@@ -22,7 +22,9 @@ public class TestDao {
 
           long end = System.currentTimeMillis();
 
-            System.out.println("Total time to execute query in milliseconds" + (end - start));
+            System.out.println("Total time "
+                    + "to execute query in milliseconds"
+                    + (end - start));
 
             while (resultSet.next()) {
                 databases.add(resultSet.getString("Database"));
