41ede31dd619bed7c8564dbf2be495d2507e782c
Martin Lynch <martin.lynch1@nhs.net>
2024-07-16 15:25:37 +0100

    Adding logging to database list

--- src/main/java/org/example/daos/TestDao.java
+++ src/main/java/org/example/daos/TestDao.java
@@ -1,6 +1,9 @@
 package org.example.daos;
 
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
