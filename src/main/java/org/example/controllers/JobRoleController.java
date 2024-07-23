2a3d000829c5e5c25244e28578a627f4b51a17d2
joel-and <joel.anderson@kainos.com>
2024-07-23 13:56:17 +0100

    fixed application

--- src/main/java/org/example/controllers/JobRoleController.java
+++ src/main/java/org/example/controllers/JobRoleController.java
@@ -11,7 +11,7 @@ import javax.ws.rs.core.Response;
 import java.sql.SQLException;
 
 @Api("Job Roles API")
-@Path("/api/")
+@Path("/api")
 
 public class JobRoleController {
     final JobRoleService jobRoleService;
@@ -22,7 +22,7 @@ public class JobRoleController {
 
 
     @GET
-    @Path("/jobRoles")
+    @Path("/job-roles")
     @Produces(MediaType.APPLICATION_JSON)
     public Response getAllJobRoles() throws SQLException {
         return Response.ok().entity(jobRoleService.getAllRoles()).build();
