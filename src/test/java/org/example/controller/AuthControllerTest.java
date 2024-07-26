3ba360b36120d10eb209960c4b553a3d9b514f3c
Adam Bowman <adam.bowman@kainos.com>
2024-07-26 12:08:56 +0100

    finished contoller tests

--- src/test/java/org/example/controller/AuthControllerTest.java
+++ src/test/java/org/example/controller/AuthControllerTest.java
@@ -1,5 +1,6 @@
 package org.example.controller;
 import org.example.controllers.AuthController;
+import org.example.daos.DatabaseConnector;
 import org.example.exceptions.DatabaseConnectionException;
 import org.example.exceptions.DoesNotExistException;
 import org.example.exceptions.InvalidException;
@@ -9,6 +10,7 @@ import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
 import javax.ws.rs.core.Response;
+import java.sql.Connection;
 import java.sql.SQLException;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -27,16 +29,90 @@ class AuthControllerTest {
     );
 
     @Test
-    void login_shouldReturnJwtToken_whenSuccessful()
+    void login_shouldReturn200_whenSuccessful()
             throws DatabaseConnectionException, SQLException,
             DoesNotExistException, InvalidException {
-        String jwtToken = "";
+        String jwtToken = "123";
 
         when(authService.login(loginRequest)).thenReturn(jwtToken);
 
         Response response = authController.login(loginRequest);
 
         assertEquals(200, response.getStatus());
+        assertEquals(jwtToken, response.getEntity());
+    }
+
+    @Test
+    void login_shouldReturn404_whenInvalid()
+            throws DatabaseConnectionException, SQLException,
+            DoesNotExistException, InvalidException {
+
+        LoginRequest loginRequest2 = new LoginRequest(
+                "notreal@random.com",
+                "password321"
+        );
+
+        when(authService.login(loginRequest2)).thenThrow(DoesNotExistException.class);
+
+        Response response = authController.login(loginRequest2);
+
+        assertEquals(404, response.getStatus());
+    }
+
+    @Test
+    void login_shouldReturn400_whenInvalid()
+            throws DatabaseConnectionException, SQLException,
+            DoesNotExistException, InvalidException {
+        LoginRequest loginRequest3 = new LoginRequest(
+                "adam@random.com",
+                "invalidPassword"
+        );
+
+        when(authService.login(loginRequest3)).thenThrow(InvalidException.class);
+
+        Response response = authController.login(loginRequest3);
+
+        assertEquals(400, response.getStatus());
+    }
+
+    @Test
+    void login_shouldReturn500_whenInvalid()
+            throws DatabaseConnectionException, SQLException,
+            DoesNotExistException, InvalidException {
+        LoginRequest loginRequest4 = new LoginRequest(
+                "",
+                ""
+        );
+
+        when(authService.login(loginRequest4)).thenThrow(SQLException.class);
+
+        Response response = authController.login(loginRequest4);
+
+        assertEquals(500, response.getStatus());
+    }
+
+    @Test
+    void generateUsers_shouldReturn200_whenSuccessful()
+            throws DatabaseConnectionException, SQLException {
+        String message = "generated two users.";
+
+        when(authService.generateUsers()).thenReturn(message);
+
+        Response response = authController.generateUsers();
+
+        assertEquals(200, response.getStatus());
+        assertEquals(message, response.getEntity());
+    }
+
+    @Test
+    void generateUser_shouldReturn500_whenInvalid()
+            throws DatabaseConnectionException, SQLException {
+
+        when(authService.generateUsers()).thenThrow(SQLException.class);
+
+        Response response = authController.generateUsers();
+
+        assertEquals(500, response.getStatus());
     }
 
 
