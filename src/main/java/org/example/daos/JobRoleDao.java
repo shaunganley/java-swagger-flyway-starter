cfe4d90af4420936d1bbc6935074ebb0c3113eb6
jackpollinsK <jackpollins@kainos.com>
2024-07-29 16:15:31 +0100

    need to add tests

--- src/main/java/org/example/daos/JobRoleDao.java
+++ src/main/java/org/example/daos/JobRoleDao.java
@@ -14,17 +14,19 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class JobRoleDao {
-    private static final Logger LOGGER = Logger.getLogger(JobRoleDao.class.getName());
+    private static final Logger LOGGER = Logger
+            .getLogger(JobRoleDao.class.getName());
 
-    public List<JobRole> getAllJobRoles(final Connection c) throws SQLException, DatabaseConnectionException {
+    public List<JobRole> getAllJobRoles(final Connection c)
+            throws SQLException, DatabaseConnectionException {
         List<JobRole> jobRoles = new ArrayList<>();
         String query = "SELECT "
-                + "jr.id, jr.role_name AS RoleName,"
-                + "l.name AS Location,"
-                + "c.name AS Capability,"
-                + "b.name AS Band,"
-                + "jr.closing_date AS ClosingDate"
-                + "FROM jobRoles jr"
+                + "jr.id, jr.role_name AS RoleName, "
+                + "l.name AS Location, "
+                + "c.name AS Capability, "
+                + "b.name AS Band, "
+                + "jr.closing_date AS ClosingDate "
+                + "FROM jobRoles jr "
                 + "INNER JOIN location l ON jr.location_id = l.id "
                 + "INNER JOIN capability c ON jr.capability_id = c.id "
                 + "INNER JOIN band b ON jr.band_id = b.id "
@@ -52,7 +54,8 @@ public class JobRoleDao {
         return jobRoles;
     }
 
-    public JobRoleInfo getJobRoleById(final int id, final Connection c) throws SQLException, DatabaseConnectionException {
+    public JobRoleInfo getJobRoleById(final int id, final Connection c)
+            throws SQLException, DatabaseConnectionException {
         String query = "SELECT "
                 + "jr.id, jr.role_name AS RoleName, "
                 + "l.name AS Location, "
@@ -88,7 +91,8 @@ public class JobRoleDao {
                 }
             }
         } catch (SQLException e) {
-            LOGGER.log(Level.SEVERE, "Error fetching job role by id: " + id, e);
+            LOGGER.log(Level.
+                    SEVERE, "Error fetching job role by id: " + id, e);
             throw e;
         }
 
