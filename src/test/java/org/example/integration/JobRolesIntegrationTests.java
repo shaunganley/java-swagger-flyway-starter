770737b07c74601834d4d9c4ffbe0e1a4ec1807d
Egal97 <egallagher97@gmail.com>
2024-07-30 11:01:57 +0100

    Backend Job Roles Integration Test

--- src/test/java/org/example/integration/JobRolesIntegrationTests.java
+++ src/test/java/org/example/integration/JobRolesIntegrationTests.java
@@ -5,11 +5,13 @@ import io.dropwizard.testing.junit5.DropwizardExtensionsSupport;
 import org.example.JDDApplication;
 import org.example.JDDConfiguration;
 import org.example.models.JobRole;
+import org.example.models.LoginRequest;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 
 import javax.ws.rs.client.Client;
+import javax.ws.rs.client.Entity;
 import java.util.List;
 
 @ExtendWith(DropwizardExtensionsSupport.class)
@@ -17,16 +19,35 @@ public class JobRolesIntegrationTests {
     public static final DropwizardAppExtension<JDDConfiguration> APP =
             new DropwizardAppExtension<>(JDDApplication.class);
 
+    private static final String EMAIL   = System.getenv("LOGIN_EMAIL_1");
+    private static final String PASSWORD  = System.getenv("LOGIN_PASSWORD_1");
+
+    private static final LoginRequest loginRequest = new LoginRequest(
+            EMAIL,
+            PASSWORD
+    );
+
     @Test
-    void getJobRoles_shouldReturnListOfJobRoles() {
+    void getJobRoles_shouldReturn401Unauthorised_WhenUserNotLoggedIn() {
         Client client = APP.client();
 
-        List<JobRole> response = client
+        int response = client
                 .target("http://localhost:8080/api/JobRoles")
                 .request()
-                .get(List.class);
+                .get().getStatus();
 
-        Assertions.assertFalse(response.isEmpty());
+        Assertions.assertEquals(401, response);
 
     }
+
+    @Test
+    void getJobRoles_shouldReturn200_WhenUserIsAuthorised() {
+        Client client = APP.client();
+        int response = client
+                .target("http://localhost:8080/api/JobRoles")
+                .request().post(Entity.json(loginRequest))
+                .getStatus();
+        Assertions.assertEquals(200,response);
+    }
+
 }
