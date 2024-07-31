136a6371cc73313a4dc20e57153cc5ea68c25336
jemorak <jemima.orakwue@hotmail.co.uk>
2024-07-31 14:05:29 +0100

    Added script to create the user table

---
+++ src/main/resources/db/migration/V2__create_user_table.sql
@@ -0,0 +1,8 @@
+CREATE TABLE user (
+    id int PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    username varchar(100) NOT NULL,
+    password varchar(100) NOT NULL,
+    role_id  int NOT NULL,
+    CONSTRAINT FK_UserRole FOREIGN KEY (id)
+    REFERENCES role(id)
+);
