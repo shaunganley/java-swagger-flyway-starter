17159712b003e0cc63373cffa11c5182af1b0a82
Joshua-Noble-Dev <joshua.noble@kainos.com>
2024-07-31 13:59:44 +0100

    Revert "010 new view role detail"

--- src/main/java/org/example/models/JobRole.java
+++ src/main/java/org/example/models/JobRole.java
@@ -6,9 +6,6 @@ import java.util.Date;
 
 public class JobRole {
 
-    public JobRole() {
-    };
-
     @JsonProperty
     private int id;
 
@@ -30,93 +27,19 @@ public class JobRole {
     @JsonProperty
     private String status;
 
-    @JsonProperty
-    private String description;
-
-    @JsonProperty
-    private String responsibilities;
-
-    @JsonProperty
-    private String jobSpec;
-
-    private JobRole(final Builder builder) {
-        this.id = builder.id;
-        this.roleName = builder.roleName;
-        this.location = builder.location;
-        this.capability = builder.capability;
-        this.band = builder.band;
-        this.closingDate = builder.closingDate;
-        this.status = builder.status;
-        this.description = builder.description;
-        this.responsibilities = builder.responsibilities;
-        this.jobSpec = builder.jobSpec;
-    }
-
-    public static class Builder {
-        private int id;
-        private String roleName;
-        private String location;
-        private String capability;
-        private String band;
-        private Date closingDate;
-        private String status;
-        private String description;
-        private String responsibilities;
-        private String jobSpec;
-
-        public Builder id(final int paramId) {
-            this.id = paramId;
-            return this;
-        }
-
-        public Builder roleName(final String paramRoleName) {
-            this.roleName = paramRoleName;
-            return this;
-        }
-
-        public Builder location(final String paramLocation) {
-            this.location = paramLocation;
-            return this;
-        }
-
-        public Builder capability(final String paramCapability) {
-            this.capability = paramCapability;
-            return this;
-        }
-
-        public Builder band(final String paramBand) {
-            this.band = paramBand;
-            return this;
-        }
-
-        public Builder closingDate(final Date paramClosingDate) {
-            this.closingDate = paramClosingDate;
-            return this;
-        }
-
-        public Builder status(final String paramStatus) {
-            this.status = paramStatus;
-            return this;
-        }
-
-        public Builder description(final String paramDescription) {
-            this.description = paramDescription;
-            return this;
-        }
-
-        public Builder responsibilities(final String paramResponsibilities) {
-            this.responsibilities = paramResponsibilities;
-            return this;
-        }
-
-        public Builder jobSpec(final String paramJobSpec) {
-            this.jobSpec = paramJobSpec;
-            return this;
-        }
-
-        public JobRole build() {
-            return new JobRole(this);
-        }
+    public JobRole(final int id, final String roleName,
+                   final String location,
+                   final String capability,
+                   final String band,
+                   final Date closingDate,
+                   final String status) {
+        this.id = id;
+        this.roleName = roleName;
+        this.location = location;
+        this.capability = capability;
+        this.band = band;
+        this.closingDate = closingDate;
+        this.status = status;
     }
 
     public int getId() {
@@ -173,29 +96,5 @@ public class JobRole {
 
     public void setStatus(final String status) {
         this.status = status;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(final String description) {
-        this.description = description;
-    }
-
-    public String getResponsibilities() {
-        return responsibilities;
-    }
-
-    public void setResponsibilities(final String responsibilities) {
-        this.responsibilities = responsibilities;
-    }
-
-    public String getJobSpec() {
-        return jobSpec;
-    }
-
-    public void setJobSpec(final String jobSpec) {
-        this.jobSpec = jobSpec;
     }
 }
--- src/main/resources/db/migration/V7__alter_role_table.sql
+++
@@ -1,8 +0,0 @@
-ALTER TABLE Role
-ADD description varchar(500);
-
-ALTER TABLE Role
-ADD responsibilities varchar(250);
-
-ALTER TABLE Role
-ADD jobSpec varchar(250);
