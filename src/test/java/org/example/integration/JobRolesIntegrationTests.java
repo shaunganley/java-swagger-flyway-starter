79a0d7e0ad377697e6319043982a485eaf7b983b
Egal97 <egallagher97@gmail.com>
2024-08-02 13:08:15 +0100

    Integration Test 1.0

--- src/test/java/org/example/integration/JobRolesIntegrationTests.java
+++ src/test/java/org/example/integration/JobRolesIntegrationTests.java
@@ -25,10 +25,16 @@ public class JobRolesIntegrationTests {
     AuthService authService = Mockito.mock(AuthService.class);
     private static final String EMAIL   = System.getenv("LOGIN_EMAIL_1");
     private static final String PASSWORD  = System.getenv("LOGIN_PASSWORD_1");
+    private static final String EMAIL2 = System.getenv("LOGIN_EMAIL_2");
+    private static final String PASSWORD2  = System.getenv("LOGIN_PASSWORD_2");
 
     private static final LoginRequest loginRequest = new LoginRequest(
             EMAIL,
             PASSWORD
+    );
+    private static final LoginRequest loginRequest2 = new LoginRequest(
+            EMAIL2,
+            PASSWORD2
     );
 
 
@@ -100,6 +106,38 @@ public class JobRolesIntegrationTests {
                         + token.readEntity(String.class)).get()
                 .getStatus();
         Assertions.assertEquals(200,response);
+    }
+
+    @Test
+    void deleteJobRole_shouldReturn403_WhenUserIsUnauthorised() {
+        Client client = APP.client();
+
+        Response token = client
+                .target("http://localhost:8080/api/auth/login")
+                .request().post(Entity.json(loginRequest));
+
+        int response = client
+                .target("http://localhost:8080/api/JobRoles/2")
+                .request().header("Authorization", "Bearer "
+                        + token.readEntity(String.class)).delete()
+                .getStatus();
+        Assertions.assertEquals(403,response);
+    }
+
+    @Test
+    void deleteJobRole_shouldReturn404_WhenUserIsAuthorisedAndDeleteNotFound() {
+        Client client = APP.client();
+
+        Response token = client
+                .target("http://localhost:8080/api/auth/login")
+                .request().post(Entity.json(loginRequest2));
+
+        int response = client
+                .target("http://localhost:8080/api/JobRoles/4")
+                .request().header("Authorization", "Bearer "
+                        + token.readEntity(String.class)).delete()
+                .getStatus();
+        Assertions.assertEquals(404,response);
     }
 
 }
