bc1295e3b2c7784f2ed452ef88dd1f9e4295cadb
Piotr Michalak <1piotr.michalak@gmail.com>
2024-08-02 15:55:16 +0200

    Refreshed Frok

--- src/main/resources/db/migration/V2__create_user_table.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE user (
-    id int PRIMARY KEY AUTO_INCREMENT NOT NULL,
-    username varchar(30) NOT NULL,
-    password varchar(50) NOT NULL,
-    roleID int,
-    FOREIGN KEY (roleID) REFERENCES role(id)
-);
