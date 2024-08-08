b4fea0c6f590d9f55d3a2d973cb237f7abab565b
TaylorB <taylor.brogan@kainos.com>
2024-08-08 11:49:21 +0100

    Updating Details

--- src/main/java/org/example/daos/DeliveryEmployeeDao.java
+++ src/main/java/org/example/daos/DeliveryEmployeeDao.java
@@ -1,4 +1,15 @@
 package org.example.daos;
 
+import org.example.models.DeliveryEmployee;
+import org.example.models.DeliveryEmployeeRequest;
+
+import java.sql.Connection;
+
 public class DeliveryEmployeeDao {
+
+    public void updateDelivery(int id, DeliveryEmployeeRequest deliveryEmployee) {
+        Connection c = databaseConnector.getConnection();
+
+        String updateStatement = "UPDATE delivery SET ";
+    }
 }
