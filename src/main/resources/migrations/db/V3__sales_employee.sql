f6670fac21b6820b1a332bd72e044945f1c1448c
Megan <meganc04@hotmail.co.uk>
2024-08-08 09:54:27 +0100

    creating sales employee table

---
+++ src/main/resources/migrations/db/V3__sales_employee.sql
@@ -0,0 +1,8 @@
+CREATE TABLE SalesEmployee(
+    salesId int PRIMARY KEY AUTO_INCREMENT,
+    name Varchar(100),
+    salary Decimal(10,2),
+    bankAccountNumber varchar(50),
+    nationalInsuranceNumber Varchar(50),
+    commissionRate Decimal(5,2)
+ );
