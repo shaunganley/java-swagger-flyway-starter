493cce49930da565c50caeedde26ff0b539bc560
JohnnyWork718 <johnny----18@hotmail.com>
2024-08-08 15:16:30 +0100

    added employee imports

--- src/main/java/org/example/daos/EmployeeDao.java
+++ src/main/java/org/example/daos/EmployeeDao.java
@@ -1,10 +1,13 @@
 package org.example.daos;
 
+import org.example.models.Employee;
+
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
+import java.util.List;
 
 public class EmployeeDao {
 
