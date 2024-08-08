08de85076a60dbc9593495a866bc8e55a96934ac
cormaco7 <cormac.okane@kainos.com>
2024-08-08 14:45:33 +0100

    added run endpoints

--- src/main/java/org/example/TestApplication.java
+++ src/main/java/org/example/TestApplication.java
@@ -5,8 +5,14 @@ import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
 import io.federecio.dropwizard.swagger.SwaggerBundle;
 import io.federecio.dropwizard.swagger.SwaggerBundleConfiguration;
+import org.example.controllers.DeliveryEmployeeController;
+import org.example.controllers.SalesEmployeeController;
 import org.example.controllers.TestController;
+import org.example.daos.DeliveryEmployeeDao;
+import org.example.daos.SalesEmployeeDao;
 import org.example.daos.TestDao;
+import org.example.services.DeliveryEmployeeService;
+import org.example.services.SalesEmployeeService;
 import org.example.services.TestService;
 
 public class TestApplication extends Application<TestConfiguration> {
@@ -35,6 +41,14 @@ public class TestApplication extends Application<TestConfiguration> {
                     final Environment environment) {
         environment.jersey()
                 .register(new TestController(new TestService(new TestDao())));
+        environment.jersey().register(
+                new DeliveryEmployeeController(
+                        new DeliveryEmployeeService(
+                                new DeliveryEmployeeDao())));
+        environment.jersey().register(
+                new SalesEmployeeController(
+                        new SalesEmployeeService(
+                                new SalesEmployeeDao())));
     }
 
 }
