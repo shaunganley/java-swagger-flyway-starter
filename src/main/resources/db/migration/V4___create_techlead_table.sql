8f1df3ef45f110de3d08c1917ca215f29baad8da
JohnnyWork718 <johnny----18@hotmail.com>
2024-08-08 11:30:40 +0100

    Changed employee to Employee on tech lead table

--- src/main/resources/db/migration/V4___create_techlead_table.sql
+++ src/main/resources/db/migration/V4___create_techlead_table.sql
@@ -1,5 +1,5 @@
 CREATE TABLE tech_lead (
             techLeadId INT AUTO_INCREMENT PRIMARY KEY,
             empId INT,
-            FOREIGN KEY (empId) REFERENCES employee(empId)
+            FOREIGN KEY (empId) REFERENCES Employee(empId)
 );
