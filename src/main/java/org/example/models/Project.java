1a91d446fb97d7671fa1801ceb4a1728f557f3e3
Pathowlll <patryk.podlewski@kainos.com>
2024-08-08 12:31:26 +0200

    creating project-model (#13)
    * creating project-model
    
    * fixing Project-model linter requirements
    
    * fixing Project-model linter requirements
    
    ---------
    
    Co-authored-by: Patryk Podlewski <patryk.podlwski@kainos.com>

---
+++ src/main/java/org/example/models/Project.java
@@ -0,0 +1,70 @@
+package org.example.models;
+
+public class Project {
+
+    private int id;
+    private String name;
+    private double value;
+    private Status status;
+    private int clientId;
+    private int teachLeadId;
+
+    public Project(int id, int clientId, Status status, String name,
+                   double value, int teachLeadId) {
+        this.id = id;
+        this.clientId = clientId;
+        this.status = status;
+        this.name = name;
+        this.value = value;
+        this.teachLeadId = teachLeadId;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+
+    public double getValue() {
+        return value;
+    }
+
+    public void setValue(double value) {
+        this.value = value;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getClientId() {
+        return clientId;
+    }
+
+    public void setClientId(int clientId) {
+        this.clientId = clientId;
+    }
+
+    public int getTeachLeadId() {
+        return teachLeadId;
+    }
+
+    public void setTeachLeadId(int teachLeadId) {
+        this.teachLeadId = teachLeadId;
+    }
+}
+
---
+++ src/main/java/org/example/models/Status.java
@@ -0,0 +1,7 @@
+package org.example.models;
+
+public enum Status {
+    IN_PROGRESS,
+    COMPLETED,
+    PENDING
+}
