17bdb0ec45f5663aef013db57bba2d8eabf36628
jericoatos <jericoatos@gmail.com>
2024-08-15 14:57:19 +0100

    Fixed validation

--- src/main/java/org/example/validators/ProjectValidator.java
+++ src/main/java/org/example/validators/ProjectValidator.java
@@ -9,8 +9,21 @@ public class ProjectValidator {
     public void validateProject(
             final ProjectDetailsRequest projectDetailsRequest)
             throws InvalidException {
+        if (projectDetailsRequest.getProjectName() == null) {
+            throw new InvalidException(Entity.PROJECT,
+                    "Project name cannot be empty");
+        }
+        if (projectDetailsRequest.getProjectValue() <= -1) {
+            throw new InvalidException(Entity.PROJECT,
+                    "Project Value must be a positive number");
+        }
         if (projectDetailsRequest.getClientId() <= -1) {
-            throw new InvalidException(Entity.PROJECT, "Client does not exist");
+            throw new InvalidException(Entity.PROJECT,
+                    "Client does not exist");
+        }
+        if (projectDetailsRequest.getTechLead() <= -1) {
+            throw new InvalidException(Entity.PROJECT,
+                    "Tech Lead does not exist");
         }
     }
 }
