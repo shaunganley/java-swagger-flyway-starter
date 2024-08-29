dca20ddb5d5caaf72f8f084b20d55a080e9fb055
Piotr S <177131800+pedrosmake@users.noreply.github.com>
2024-08-29 15:49:38 +0200

    US040: Add JwtUtils class and use in TestApplication.

---
+++ src/main/java/utils/JwtUtils.java
@@ -0,0 +1,34 @@
+package org.example.utils;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+
+import javax.crypto.SecretKey;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+public class JwtUtils {
+    // Generate a SecretKey for HS256
+    private static final SecretKey SECRET_KEY = Keys.secretKeyFor(SignatureAlgorithm.HS256);
+    private static final long EXPIRATION_TIME = 86400000; // 1 day in milliseconds
+
+    public static String generateToken(String email) {
+        Map<String, Object> claims = new HashMap<>();
+        claims.put("email", email);
+
+        return Jwts.builder()
+                .setClaims(claims)
+                .setSubject(email)
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
+                .signWith(SECRET_KEY, SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    public static SecretKey getSecretKey() {
+        return SECRET_KEY;
+    }
+}
+
