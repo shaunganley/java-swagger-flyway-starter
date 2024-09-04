4aa62db02bdee46fb0a06ea1103eda68afe894ef
Patryk Podlewski <patryk.podlewski@kainos.com>
2024-09-04 10:34:43 +0200

    US028: adding method assertEqualList() for testing

--- src/test/java/org/example/utils/Utils.java
+++ src/test/java/org/example/utils/Utils.java
@@ -6,15 +6,19 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class Utils {
 
-    public static <T, G> void compareLists(List<T> list1, List<G> list2) {
-        if (list1.size() != list2.size()) {
-            fail();
+    public static <T, G> void assertEqualLists(List<T> list1, List<G> list2) {
+        if (list1 == null) {
+            fail("First list is null");
         }
-
+        if (list2 == null) {
+            fail("Second list is null");
+        }
+        if (list1.size() != list2.size()) {
+            fail("Lists are different size.");
+        }
         for (int i = 0; i < list1.size(); i++) {
             assertEquals(list1.get(i), list2.get(i));
         }
-
     }
 
 }
