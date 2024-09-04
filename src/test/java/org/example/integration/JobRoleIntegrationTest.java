d4686cd64ed4ad32ad75ec25f8f713a2168199bf
jericoatos <jericoatos@gmail.com>
2024-09-04 15:37:22 +0100

    Fixed job role integration test to include login token

--- src/test/java/org/example/integration/JobRoleIntegrationTest.java
+++ src/test/java/org/example/integration/JobRoleIntegrationTest.java
@@ -5,12 +5,18 @@ import io.dropwizard.testing.junit5.DropwizardExtensionsSupport;
 import org.example.KainosJobWebApplication;
 import org.example.KainosJobWebConfiguration;
 import org.example.models.JobRole;
+import org.example.models.LoginRequest;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 
 import javax.ws.rs.client.Client;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.core.Response;
 import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
 @ExtendWith(DropwizardExtensionsSupport.class)
 public class JobRoleIntegrationTest {
@@ -18,13 +24,30 @@ public class JobRoleIntegrationTest {
     private static final DropwizardAppExtension<KainosJobWebConfiguration> APP =
             new DropwizardAppExtension<>(KainosJobWebApplication.class);
 
+    private String loginAndGetToken() {
+        Client client = APP.client();
+        Response response =
+                client.target("http://localhost:8080/api/auth/login")
+                        .request()
+                        .post(Entity.json(new LoginRequest(
+                                System.getenv().get("VALID_TEST_EMAIL"),
+                                System.getenv().get("VALID_TEST_PASSWORD")
+                        )));
+        assertEquals(Response.Status.OK.getStatusCode(), response.getStatus());
+        String token = response.readEntity(String.class);
+        assertNotNull(token);
+        return token;
+    }
+
+
     @Test
     public void getJobRoles_shouldReturnListOfJobRoles() {
         Client client = APP.client();
-
+        String token = loginAndGetToken();
         List<JobRole> response = client
                 .target("http://localhost:8080/api/job-roles")
                 .request()
+                .header("Authorization", "Bearer " + token)
                 .get(List.class);
 
         Assertions.assertFalse(response.isEmpty());
