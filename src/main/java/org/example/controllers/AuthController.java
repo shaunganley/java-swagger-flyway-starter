be05f8eb551e775b71144803b4c26e12f9e9d7db
jericoatos <jericoatos@gmail.com>
2024-09-05 08:50:28 +0100

    Linting issues

--- src/main/java/org/example/controllers/AuthController.java
+++ src/main/java/org/example/controllers/AuthController.java
@@ -1,9 +1,6 @@
 package org.example.controllers;
 
 import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiKeyAuthDefinition;
-import io.swagger.annotations.SecurityDefinition;
-import io.swagger.annotations.SwaggerDefinition;
 import org.example.exceptions.DatabaseConnectionException;
 import org.example.exceptions.LoginException;
 import org.example.models.LoginRequest;
@@ -14,7 +11,6 @@ import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 
-import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.sql.SQLException;
