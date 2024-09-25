6dabbf9b8adc06e5869f28436ac51c479cda5056
Dylan Branda <dylan.branda@hkgk33wkq6.home>
2024-09-25 16:02:57 +0100

    Fixed syntax error

--- src/main/resources/db/migration/V2__create_user_table.sql
+++
@@ -1,6 +0,0 @@
-create table user (
-    id int primary key auto_increment not null,
-    username varchar(100) not null,
-    password varchar(100) not null,
-    role_id int foreign key not null
-);
