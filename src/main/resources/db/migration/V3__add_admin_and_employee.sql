2455ad5bbd9bbefc993ecaa9f7c985d07f109ff0
Dan Adams <daniel.adams@kainos.com>
2024-09-25 15:56:39 +0100

    added admin and employee script

---
+++ src/main/resources/db/migration/V3__add_admin_and_employee.sql
@@ -0,0 +1,4 @@
+INSERT INTO `role` (id, title) VALUES ('1', 'Admin');
+INSERT INTO `role` (id, title) VALUES ('2', 'Employee');
+INSERT INTO `user` (id, username, password, roleId) VALUES ('1', 'Don McFall', 'vrp', '1');
+INSERT INTO `user` (id, username, password, roleId) VALUES ('2', 'Connor Forde', 'nose', '2');
