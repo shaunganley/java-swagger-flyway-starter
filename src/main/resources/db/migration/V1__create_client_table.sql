f4494da4338c845492fb70c50b15cbeac5753d82
Dan Adams <daniel.adams@kainos.com>
2024-09-26 16:02:41 +0100

    JDB-001 disallow name, address and phone to be null in client table migration

--- src/main/resources/db/migration/V1__create_client_table.sql
+++ src/main/resources/db/migration/V1__create_client_table.sql
@@ -1,6 +1,6 @@
 CREATE TABLE `client` (
     id int AUTO_INCREMENT NOT NULL,
-    name nvarchar(50),
-    address nvarchar(50),
-    phone nvarchar(15)
+    name nvarchar(50) NOT NULL,
+    address nvarchar(50) NOT NULL,
+    phone nvarchar(15) NOT NULL
 )
