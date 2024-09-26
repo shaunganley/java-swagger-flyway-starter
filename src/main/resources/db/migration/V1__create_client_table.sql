40fa20adaec62f36d2ce95f2a91774c17fc96967
Dan Adams <daniel.adams@kainos.com>
2024-09-26 16:39:16 +0100

    JDB-001 renamed id to clientId for better readability

--- src/main/resources/db/migration/V1__create_client_table.sql
+++ src/main/resources/db/migration/V1__create_client_table.sql
@@ -1,14 +1,11 @@
 CREATE TABLE `client` (
-    id int AUTO_INCREMENT NOT NULL,
+    clientId int AUTO_INCREMENT NOT NULL,
     name nvarchar(50) NOT NULL,
-
-    # Address
-    street1 nvarchar(50) NOT NULL,
-    street2 nvarchar(50),
+    addressLine1 nvarchar(50) NOT NULL,
+    addressLine2 nvarchar(50),
     town nvarchar(50),
     city nvarchar(50),
     postcode nvarchar(10),
     country char(2) NOT NULL
-
     phone varchar(15)
 )
