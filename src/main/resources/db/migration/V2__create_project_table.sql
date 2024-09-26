9fc3ff996fcd7ef4a2d101c3ca418be8422acfbc
jamesliambutler <james.liam.butler@kainos.com>
2024-09-26 16:14:20 +0100

    create project table

---
+++ src/main/resources/db/migration/V2__create_project_table.sql
@@ -0,0 +1,10 @@
+CREATE TABLE Project (
+    `name` varchar(50),
+    `value` decimal(9, 2),
+    technologyId int,
+    techLeadId int,
+    clientId int,
+    salesEmployeeID int,
+    startDate DateTime,
+    finishDate DateTime
+);
