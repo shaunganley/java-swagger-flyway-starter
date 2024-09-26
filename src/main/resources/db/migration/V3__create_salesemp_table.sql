d99a44598bd0d2025b6e3be73a3bdb0ad2ec7646
dylanmckk <dylan.mckenzie@kainos.com>
2024-09-26 16:56:53 +0100

    JTD-004 created sales employee table

---
+++ src/main/resources/db/migration/V3__create_salesemp_table.sql
@@ -0,0 +1,11 @@
+CREATE TABLE SalesEmployee (
+    salesEmployeeID int AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    firstName nvarchar(50) NOT NULL,
+    lastName nvarchar(50) NOT NULL,
+    salary decimal NOT NULL,
+    currency char(3) NOT NULL,
+    bankAccountNo varchar(34) NOT NULL,
+    nationalInsuranceNo char(9) NOT NULL,
+    commissionRate decimal(3,2) NOT NULL,
+    CHECK (commissionRate<=1)
+)
