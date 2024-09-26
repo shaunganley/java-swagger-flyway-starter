f704428eb108abb821be6a0fe646d5fa6a66ac14
Dan Adams <daniel.adams@kainos.com>
2024-09-26 16:23:17 +0100

    JDB-001 added columns for street address, town, city, zip/postcode and ISO3166 country code

--- src/main/resources/db/migration/V1__create_client_table.sql
+++ src/main/resources/db/migration/V1__create_client_table.sql
@@ -1,6 +1,14 @@
 CREATE TABLE `client` (
     id int AUTO_INCREMENT NOT NULL,
     name nvarchar(50) NOT NULL,
-    address nvarchar(50) NOT NULL,
-    phone nvarchar(15) NOT NULL
+
+    # Address
+    street1 nvarchar(50) NOT NULL,
+    street2 nvarchar(50),
+    town nvarchar(50),
+    city nvarchar(50),
+    zip nvarchar(10) NOT NULL,
+    country char(2) NOT NULL
+
+    phone varchar(15)
 )
