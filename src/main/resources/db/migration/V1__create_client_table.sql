81928d10d3c4387cfbdb5f9c1c29dbca4df3374f
Dan Adams <daniel.adams@kainos.com>
2024-09-27 09:42:59 +0100

    JTB-020 change nvarchar to varchar to remove deprecation warnings

--- src/main/resources/db/migration/V1__create_client_table.sql
+++ src/main/resources/db/migration/V1__create_client_table.sql
@@ -1,11 +1,11 @@
 CREATE TABLE `client` (
     clientId int AUTO_INCREMENT NOT NULL PRIMARY KEY,
     name nvarchar(50) NOT NULL,
-    addressLine1 nvarchar(50) NOT NULL,
-    addressLine2 nvarchar(50),
-    town nvarchar(50),
-    city nvarchar(50),
-    postcode nvarchar(10),
+    addressLine1 varchar(50) NOT NULL,
+    addressLine2 varchar(50),
+    town varchar(50),
+    city varchar(50),
+    postcode varchar(10),
     country char(2) NOT NULL,
     phone varchar(15)
 )
