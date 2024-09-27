2cc45e8fd069ccf20c380cf41fc24c3f2b753024
dylanmckk <dylan.mckenzie@kainos.com>
2024-09-27 09:05:59 +0100

    Update V3__create_salesemp_table.sql

--- src/main/resources/db/migration/V3__create_salesemp_table.sql
+++ src/main/resources/db/migration/V3__create_salesemp_table.sql
@@ -1,11 +1,11 @@
-CREATE TABLE SalesEmployee (
-    salesEmployeeID int AUTO_INCREMENT NOT NULL PRIMARY KEY,
+CREATE TABLE salesEmployees (
+    salesEmployeeId int AUTO_INCREMENT NOT NULL PRIMARY KEY,
     firstName nvarchar(50) NOT NULL,
     lastName nvarchar(50) NOT NULL,
     salary decimal NOT NULL,
     currency char(3) NOT NULL,
     bankAccountNo varchar(34) NOT NULL,
-    nationalInsuranceNo char(9) NOT NULL,
+    nationalInsuranceNo char(9),
     commissionRate decimal(3,2) NOT NULL,
     CHECK (commissionRate<=1)
 )
