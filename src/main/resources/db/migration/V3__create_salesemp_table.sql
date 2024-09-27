588d8def75a3dfc3448c3068016054e4703759f7
dylanmckk <dylan.mckenzie@kainos.com>
2024-09-27 09:41:20 +0100

    Update V3__create_salesemp_table.sql
    removed cr, added decimal size to salary

--- src/main/resources/db/migration/V3__create_salesemp_table.sql
+++ src/main/resources/db/migration/V3__create_salesemp_table.sql
@@ -2,10 +2,8 @@ CREATE TABLE salesEmployees (
     salesEmployeeId int AUTO_INCREMENT NOT NULL PRIMARY KEY,
     firstName nvarchar(50) NOT NULL,
     lastName nvarchar(50) NOT NULL,
-    salary decimal NOT NULL,
+    salary decimal(10,2) NOT NULL,
     currency char(3) NOT NULL,
     bankAccountNo varchar(34) NOT NULL,
     nationalInsuranceNo char(9),
-    commissionRate decimal(3,2) NOT NULL,
-    CHECK (commissionRate<=1)
 )
