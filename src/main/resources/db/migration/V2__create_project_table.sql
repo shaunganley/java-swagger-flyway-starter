11e1b234b8241a5732e68f0a4d93404cf7cce3b6
james-liam-butler <james.liam.butler@kainos.com>
2024-09-27 09:26:36 +0100

    Add commission rate to project table

--- src/main/resources/db/migration/V2__create_project_table.sql
+++ src/main/resources/db/migration/V2__create_project_table.sql
@@ -1,12 +1,13 @@
 CREATE TABLE project (
     projectId int NOT NULL AUTO_INCREMENT,
     `name` varchar(50),
-    `value` decimal(9, 2),
+    `value` decimal(11, 2),
     technologyId int,
     techLeadId int,
     clientId int,
     salesEmployeeID int,
     startDate DateTime,
     finishDate DateTime,
+    commissionRate decimal(3, 2),
     PRIMARY KEY(projectId)
 );
