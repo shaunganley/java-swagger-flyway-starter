c97649a12949a2f091b06d6646cbefd67fdbfafa
dylanmckk <dylan.mckenzie@kainos.com>
2024-09-27 09:58:59 +0100

    Update V3__create_salesemp_table.sql
    changed nvarchar to varchar

--- src/main/resources/db/migration/V3__create_salesemp_table.sql
+++ src/main/resources/db/migration/V3__create_salesemp_table.sql
@@ -1,7 +1,7 @@
 CREATE TABLE salesEmployees (
     salesEmployeeId int AUTO_INCREMENT NOT NULL PRIMARY KEY,
-    firstName nvarchar(50) NOT NULL,
-    lastName nvarchar(50) NOT NULL,
+    firstName varchar(50) NOT NULL,
+    lastName varchar(50) NOT NULL,
     salary decimal(10,2) NOT NULL,
     currency char(3) NOT NULL,
     bankAccountNo varchar(34) NOT NULL,
