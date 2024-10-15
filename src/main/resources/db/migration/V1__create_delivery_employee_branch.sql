45b75f87edf98be11cdf2c37b79c6f5996ba5593
james-liam-butler <james.liam.butler@kainos.com>
2024-10-15 14:52:05 +0100

    AMB-02 Capitalize data types and remove currency

--- src/main/resources/db/migration/V1__create_delivery_employee_branch.sql
+++ src/main/resources/db/migration/V1__create_delivery_employee_branch.sql
@@ -1,9 +1,8 @@
 CREATE TABLE deliveryEmployee (
-	deliveryEmployeeId int NOT NULL AUTO_INCREMENT,
-	`name` varchar(50),
-	salary decimal(7, 2),
-	currency char(3),
-	bankAccountNumber varchar(34),
-	nationalInsuranceNumber varchar(9),
+	deliveryEmployeeId INT NOT NULL AUTO_INCREMENT,
+	`name` VARCHAR(50),
+	salary DECIMAL(7, 2),
+	bankAccountNumber VARCHAR(34),
+	nationalInsuranceNumber VARCHAR(9),
 	PRIMARY KEY(deliveryEmployeeId)
 );
