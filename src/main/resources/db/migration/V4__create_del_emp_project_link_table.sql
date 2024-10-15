0fd93145a3e55029cfce4b17d1714480f8f56225
jamesliambutler <james.liam.butler@kainos.com>
2024-10-15 15:52:29 +0100

    AMB-007-Capitalize_data_typers

--- src/main/resources/db/migration/V4__create_del_emp_project_link_table.sql
+++ src/main/resources/db/migration/V4__create_del_emp_project_link_table.sql
@@ -1,6 +1,6 @@
 CREATE TABLE projectDeliveryEmployees (
-    projectId int NOT NULL,
-    deliveryEmployeeId int NOT NULL,
+    projectId INT NOT NULL,
+    deliveryEmployeeId INT NOT NULL,
     employeeStartDate DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
     employeeFinishDate DATETIME,
     FOREIGN KEY(projectId) REFERENCES project(projectId),
