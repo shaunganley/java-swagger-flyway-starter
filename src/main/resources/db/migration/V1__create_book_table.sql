d2ab9d014e3a0001217a953021614af7452b354f
jamesliambutler <james.liam.butler@kainos.com>
2024-10-15 11:01:04 +0100

    LBJ-1-Add-create-book-table-migration

---
+++ src/main/resources/db/migration/V1__create_book_table.sql
@@ -0,0 +1,11 @@
+CREATE TABLE Books (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    author VARCHAR(255) NOT NULL,
+    publisher VARCHAR(255) NOT NULL,
+    isbn VARCHAR(255) NOT NULL,
+    publication_year YEAR NOT NULL,
+    genre VARCHAR(255) NOT NULL,
+    available TINYINT(1) NOT NULL DEFAULT 1,
+    price DECIMAL(10,2) NOT NULL
+);
