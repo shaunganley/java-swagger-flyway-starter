cabc4a837e04b07ad26170e7aa4026d9e688c913
Aaron-Millen <aaron.millen@kainos.com>
2024-10-15 15:52:48 +0100

    Update V3__create_sales_employee.sql

--- src/main/resources/db/migration/V3__create_sales_employee.sql
+++ src/main/resources/db/migration/V3__create_sales_employee.sql
@@ -3,7 +3,7 @@ CREATE TABLE salesEmployees (
     firstName VARCHAR(50) NOT NULL,
     lastName VARCHAR(50) NOT NULL,
     salary DECIMAL(10,2) NOT NULL,
-    bankAccountNo VARCHAR(34) NOT NULL,
-    nationalInsuranceNo CHAR(9),
+    bankAccountNumber VARCHAR(34) NOT NULL,
+    nationalInsuranceNumber CHAR(9),
     PRIMARY KEY(salesEmployeeId)
 );
