e71bd1e7cf0a13c90c766c8a71efe5238d1e7c3e
Dan Adams <daniel.adams@kainos.com>
2024-10-16 09:44:06 +0100

    AMB-21 Added get client by ID method

--- src/main/java/org/soniakbew/daos/ClientDao.java
+++ src/main/java/org/soniakbew/daos/ClientDao.java
@@ -3,6 +3,7 @@ package org.soniakbew.daos;
 import org.soniakbew.models.Client;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -10,7 +11,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ClientDao {
-    List<Client> getAllClients() throws SQLException {
+    public List<Client> getAllClients() throws SQLException {
         List<Client> clients = new ArrayList<>();
 
         try (Connection connection = DatabaseConnector.getConnection()) {
@@ -29,5 +30,23 @@ public class ClientDao {
             }
         }
         return clients;
+    }
+
+    public Client getClientById(final int clientId) throws SQLException {
+        try (Connection connection = DatabaseConnector.getConnection()) {
+            String query =
+                    "SELECT clientId, name FROM `client` WHERE clientId = ?;";
+            PreparedStatement statement = connection.prepareStatement(query);
+            statement.setInt(1, clientId);
+
+            ResultSet resultSet = statement.executeQuery();
+            if (resultSet.next()) {
+                return new Client(
+                        resultSet.getInt("clientId"),
+                        resultSet.getString("name")
+                );
+            }
+        }
+        return null;
     }
 }
