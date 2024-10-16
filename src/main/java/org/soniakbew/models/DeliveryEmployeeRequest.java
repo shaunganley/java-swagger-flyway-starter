eb04e38be013e5a918038f82728b6fad0986034f
jamesliambutler <james.liam.butler@kainos.com>
2024-10-16 09:40:17 +0100

    AMB-011_Created_delivery_employee_request_class

---
+++ src/main/java/org/soniakbew/models/DeliveryEmployeeRequest.java
@@ -0,0 +1,64 @@
+package org.soniakbew.models;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class DeliveryEmployeeRequest {
+    private String name;
+    private Double salary;
+    private String bankAccountNumber;
+    private String nationalInsuranceNumber;
+
+    @JsonCreator
+    public DeliveryEmployeeRequest(
+            final @JsonProperty("name") String name,
+            final @JsonProperty("salary") Double salary,
+            final @JsonProperty("bankAccountNumber") String bankAccountNumber,
+            final @JsonProperty(
+                    "nationalInsuranceNumber"
+            ) String nationalInsuranceNumber
+    ) {
+        this.name = name;
+        this.salary = salary;
+        this.bankAccountNumber = bankAccountNumber;
+        this.nationalInsuranceNumber = nationalInsuranceNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(final String name) {
+        this.name = name;
+    }
+
+    public Double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(final Double salary) {
+        this.salary = salary;
+    }
+
+    public String getBankAccountNumber() {
+        return bankAccountNumber;
+    }
+
+    public void setBankAccountNumber(final String bankAccountNumber) {
+        this.bankAccountNumber = bankAccountNumber;
+    }
+
+    public String getNationalInsuranceNumber() {
+        return nationalInsuranceNumber;
+    }
+
+    public void setNationalInsuranceNumber(
+            final String nationalInsuranceNumber
+    ) {
+        this.nationalInsuranceNumber = nationalInsuranceNumber;
+    }
+}
+
+
+
+
