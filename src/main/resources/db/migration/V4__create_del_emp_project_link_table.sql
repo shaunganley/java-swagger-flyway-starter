12487e9fd5f83b06fef2941e046d9dff15610aa2
james-liam-butler <james.liam.butler@kainos.com>
2024-10-16 09:10:42 +0100

    AMB-07 Remove the start and finish date

--- src/main/resources/db/migration/V4__create_del_emp_project_link_table.sql
+++ src/main/resources/db/migration/V4__create_del_emp_project_link_table.sql
@@ -1,8 +1,6 @@
 CREATE TABLE projectDeliveryEmployees (
     projectId INT NOT NULL,
     deliveryEmployeeId INT NOT NULL,
-    employeeStartDate DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    employeeFinishDate DATETIME,
     FOREIGN KEY(projectId) REFERENCES project(projectId),
     FOREIGN KEY(deliveryEmployeeId) REFERENCES deliveryEmployee(deliveryEmployeeId),
     PRIMARY KEY(projectId, deliveryEmployeeId)
