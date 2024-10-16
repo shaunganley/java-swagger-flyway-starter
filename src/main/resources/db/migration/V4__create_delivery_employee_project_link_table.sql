bd786953ec27731a3cca403d3e7931feaafc1df1
james-liam-butler <james.liam.butler@kainos.com>
2024-10-16 09:55:15 +0100

    AMB-07 Change order of primary and foreign key

--- src/main/resources/db/migration/V4__create_delivery_employee_project_link_table.sql
+++ src/main/resources/db/migration/V4__create_delivery_employee_project_link_table.sql
@@ -1,7 +1,8 @@
 CREATE TABLE projectDeliveryEmployees (
     projectId INT NOT NULL,
     deliveryEmployeeId INT NOT NULL,
+    PRIMARY KEY(projectId, deliveryEmployeeId),
     FOREIGN KEY(projectId) REFERENCES project(projectId),
     FOREIGN KEY(deliveryEmployeeId) REFERENCES deliveryEmployee(deliveryEmployeeId),
-    PRIMARY KEY(projectId, deliveryEmployeeId)
+
 );
