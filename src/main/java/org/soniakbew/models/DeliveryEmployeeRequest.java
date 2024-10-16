2009ca649dd0ba0f807f52b3faf367be6705ebe7
jamesliambutler <james.liam.butler@kainos.com>
2024-10-16 10:20:33 +0100

    Remove getters and setters

--- src/main/java/org/soniakbew/models/DeliveryEmployeeRequest.java
+++ src/main/java/org/soniakbew/models/DeliveryEmployeeRequest.java
@@ -4,10 +4,10 @@ import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 public class DeliveryEmployeeRequest {
-    private String name;
-    private Double salary;
-    private String bankAccountNumber;
-    private String nationalInsuranceNumber;
+    private final String name;
+    private final Double salary;
+    private final String bankAccountNumber;
+    private final String nationalInsuranceNumber;
 
     @JsonCreator
     public DeliveryEmployeeRequest(
@@ -21,40 +21,6 @@ public class DeliveryEmployeeRequest {
         this.name = name;
         this.salary = salary;
         this.bankAccountNumber = bankAccountNumber;
-        this.nationalInsuranceNumber = nationalInsuranceNumber;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(final String name) {
-        this.name = name;
-    }
-
-    public Double getSalary() {
-        return salary;
-    }
-
-    public void setSalary(final Double salary) {
-        this.salary = salary;
-    }
-
-    public String getBankAccountNumber() {
-        return bankAccountNumber;
-    }
-
-    public void setBankAccountNumber(final String bankAccountNumber) {
-        this.bankAccountNumber = bankAccountNumber;
-    }
-
-    public String getNationalInsuranceNumber() {
-        return nationalInsuranceNumber;
-    }
-
-    public void setNationalInsuranceNumber(
-            final String nationalInsuranceNumber
-    ) {
         this.nationalInsuranceNumber = nationalInsuranceNumber;
     }
 }
