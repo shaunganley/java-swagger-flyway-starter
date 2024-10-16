04962bb8e8d9e0b01d9a969ffe13e6b0c2442bfe
Dan Adams <daniel.adams@kainos.com>
2024-10-16 11:04:44 +0100

    AMB-20 fixed empty files

--- src/main/java/org/soniakbew/controllers/ClientController.java
+++ src/main/java/org/soniakbew/controllers/ClientController.java
@@ -1,4 +1,10 @@
 package org.soniakbew.controllers;
 
+import org.soniakbew.services.ClientService;
+
 public class ClientController {
+    ClientService clientService;
+    public ClientController(final ClientService clientService) {
+        this.clientService = clientService;
+    }
 }
