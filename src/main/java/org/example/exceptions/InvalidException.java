c6eec8d2c205f278d762480b4effb857abf74e29
aaron.millen <aaron.millen@kainos.com>
2024-11-06 16:30:34 +0000

    YJ_004: Added Sales Employee DAO and Validator files

---
+++ src/main/java/org/example/exceptions/InvalidException.java
@@ -0,0 +1,7 @@
+package org.example.exceptions;
+
+public class InvalidException extends RuntimeException {
+  public InvalidException(String message) {
+    super(message);
+  }
+}
---
+++ src/main/resources/db/migration/V3__create_sales_employee_table.sql
@@ -0,0 +1,7 @@
+SalesEmployee (
+	employeeId int NOT NULL,
+	commissionRate decimal(4,3) NOT NULL,
+	CHECK (commissionRate<=1),
+	PRIMARY KEY (employeeId),
+	FOREIGN KEY (employeeId) REFERENCES Employee(employeeId)
+);
